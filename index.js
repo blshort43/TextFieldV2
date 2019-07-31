418670cdcc87f465966c8c5b972de133a70930f5
Brandon <blshort43@users.noreply.github.com>
2019-07-31 16:13:50 -0500

    fixed componentDidMount
    Fields with value !== '' will be focused on componentDidMount

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ class TextField extends React.PureComponent {
   };
 
   componentDidMount() {
-    if (this.props.type === 'date') {
+    if (this.props.value !== '') {
       this.handleFocus();
     }
   }
